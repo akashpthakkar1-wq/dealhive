e20b184eecf24697e137cb6b0193c37f9dc7caed
Akash Thakkar <akash.p.thakkar1@gmail.com>
2026-04-08 23:16:50 +0530

    fix footer logo

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
           {/* Brand */}
           <div className="lg:col-span-1">
             <Link href="/" className="flex items-center gap-2 mb-4">
-              <img src="/logo.svg" alt="EndOverPay" className="h-10 w-auto brightness-0 invert" />
+              <img src="/logo.svg" alt="EndOverPay" className="h-10 w-auto" />
             </Link>
             <p className="text-sm text-gray-400 leading-relaxed mb-5">
               India&apos;s most trusted coupon & deal platform. Verified codes, real savings.
